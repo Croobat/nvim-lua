515fb9c8ab2e7bfd7a8984a888a1394deee3543f
Tony Ramírez <tonyram9906@outlook.com>
2022-06-07 22:04:18 -0500

    basic modifications

---
+++ lua/user/options.lua
@@ -0,0 +1,58 @@
+-- :help options
+--
+
+o = vim.opt
+
+local options = {
+  backup = false,                          -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- `` visible md files
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = false,                        -- highlight matches prev search
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                             -- allow mouse
+  pumheight = 10,                          -- smaller pop up menu height
+  showmode = false,                        --  -- INSERT -- 
+  showtabline = 1,                         -- show tabs if >=2
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- horizontal splits to go below current window
+  splitright = true,                       -- vertical splits to go to the right of current window
+  swapfile = false,                       -- creates a swapfile
+  termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 400,                        -- time to wait for a mapped sequence to complete
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited it cant be edited at same time
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  tabstop = 2,                             -- insert 2 spaces for a tab
+  cursorline = true,                       -- highlight the current line
+  number = true,                           -- set numbered lines
+  relativenumber = false,                  -- set relative numbered lines
+  numberwidth = 2,                         -- set number column width to 2 {default 4}
+  signcolumn = "yes",                      -- always show the sign colum
+  wrap = true,                            -- display lines as one long line
+  scrolloff = 8,                           -- minimum distance cursor-border
+  sidescrolloff = 8,
+  guifont = "JetBrainsMono Nerd Font:h18", -- the font used in graphical neovim applications
+  fdm = "marker",				-- Marker fold method {{{}}}
+}
+--formatoptions-=cro,			-- Stop newline comment continuation
+o.shm:append "I"				-- Start screen
+o.shortmess:append "T" 			-- Truncate long messages
+o.shortmess:append "c"			-- Dont give ins-completion menu
+o.list.lcs = "tab:|"
+
+for k, v in pairs(options) do -- abreviando vim.opt.x con local options y o
+  o[k] = v
+end
+
+-- laststatus = 2
+-- statusline = %m\ %F\ %y\ %{&fileencoding?&fileencoding:&encoding}\ %=%(C:%c\ L:%l\ %P%)
+
+vim.cmd "set whichwrap+=<,>,[,],h,l" 		
+vim.cmd [[set iskeyword+=-]] 			-- separate words by dashes
+vim.cmd [[set formatoptions-=cro]]	        -- TODO: this doesn't seem to work
