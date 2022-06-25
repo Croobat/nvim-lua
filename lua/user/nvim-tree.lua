29d1fa10d269050254e896cb7ce5d448a0a802da
Tony Ramírez <tonyram9906@outlook.com>
2022-06-24 23:08:11 -0500

    added nvim-tree bindings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -68,6 +68,8 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
+        { key = "I", cb = tree_cb "full_rename" },
+        { key = "A", cb = tree_cb "rename" },
       },
     },
     number = false,
