9e634f6fee41e899437f362d8d356498a32f24e5
Tony Ramírez <tonyram9906@outlook.com>
2022-06-25 10:50:14 -0500

    benchmarking

--- init.lua
+++ init.lua
@@ -1,18 +1,24 @@
-require "user.options"	        -- General options
-require "user.keymaps"          -- Key bindings
-require "user.plugins"          -- Plugins
-require "user.colorscheme"      -- Color scheme
+require "user.options"	        -- General options 6.2ms
+require "user.keymaps"          -- Key bindings <5ms
+require "user.plugins"          -- Plugins <5ms
+require "user.colorscheme"      -- Color scheme <5ms
 
 -- Plugin configurations
-require "user.cmp"              -- Completion config
-require "user.lsp"              -- LSP config
-require "user.telescope"        -- Fuzzy finder
-require "user.treesitter"       -- Better syntax highlighting
-require "user.nvim-colorizer"   -- Hex colors
-require "user.autopairs"        -- Auto close parens
-require "user.comment"          -- Smart comments
-require "user.gitsigns"         -- Git symbols
-require "user.nvim-tree"        -- better file explorer
-require "user.bufferline"       -- buffer tabs
-require "user.lualine"          -- custom info line
-require "user.toggleterm"       -- toggle terminal
+-- >10ms
+require "user.telescope"        -- Fuzzy finder 13.5ms
+require "user.nvim-tree"        -- better file explorer 14ms
+
+
+-- 5-10ms
+require "user.lsp"              -- LSP config 6ms
+require "user.treesitter"       -- Better syntax highlighting 8ms
+require "user.autopairs"        -- Auto close parens 6ms
+require "user.gitsigns"         -- Git symbols 6ms
+require "user.lualine"          -- custom info line 6.5ms
+
+-- <5ms
+require "user.cmp"              -- Completion config <5ms
+require "user.nvim-colorizer"   -- Hex colors <5ms
+require "user.comment"          -- Smart comments <5ms
+require "user.bufferline"       -- buffer tabs <5ms
+require "user.toggleterm"       -- toggle terminal <5ms
--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -92,10 +92,6 @@
   -- Telescope
   keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
   keymap("n", "<leader>t", "<cmd>Telescope live_grep theme=ivy<cr>", opts)
-  --keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
-  --keymap("n", "<leader>t", "<cmd>Telescope live_grep<cr>", opts)
-  --keymap("n", "<leader>f", "<cmd>Telescope find_files theme=ivy<cr>", opts)
-  --keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 
   -- Gitsigns
   keymap("n", "<leader>gh", ":Gitsigns next_hunk<cr>", opts)
