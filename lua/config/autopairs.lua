51a053ba9379f217de20bf8b3343b71f3acb7881
Tony Ramírez <tonyram9906@outlook.com>
2022-06-30 21:03:18 -0500

    nvim-autopairs added auto space and arrow function

--- lua/config/autopairs.lua
+++ lua/config/autopairs.lua
@@ -1,4 +1,4 @@
--- Setup nvim-cmp.
+-- Setup auto-pairs{{{
 local status_ok, npairs = pcall(require, "nvim-autopairs")
 if not status_ok then
   return
@@ -23,11 +23,49 @@ npairs.setup {
     highlight = "PmenuSel",
     highlight_grey = "LineNr",
   },
-}
+}--}}}
 
+-- Setup cmp integration{{{
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
   return
 end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })--}}}
+
+-- ╭──────────────╮
+-- │ Custom rules │
+-- ╰──────────────╯
+local Rule   = require'nvim-autopairs.rule'
+
+-- Auto space
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ', ' )')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+  Rule('{ ', ' }')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+  Rule('[ ', ' ]')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']'),
+
+  -- Arrow function (js)
+  Rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
+        :use_regex(true)
+        :set_end_pair_length(2),
+}
